9239ab28e03c84a0dabea053140e9d5d40473d47
opsdisk <brennon.thomas@opsdisk.com>
2016-01-09 11:50:40 -0600

    Added threaded download capability, print() function, and small aesthetic changes

--- metagoofil.py
+++ metagoofil.py
@@ -1,19 +1,49 @@
 #!/usr/bin/env python
 # GPL v 2.0 License
 # Opsdisk LLC | opsdisk.com
+from __future__ import print_function
 
 import argparse
 import google  # https://pypi.python.org/pypi/google
 import os
+import Queue
 import sys
+import threading
 import time
 import urllib
 import urllib2
 
 
+class Worker(threading.Thread):
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+        
+    def run(self):
+        while True:          
+            # Grab URL off the queue
+            url = mg.queue.get()
+            try:
+                response = urllib2.urlopen(url, timeout=5)
+                # Determine if file is small enough to download
+                size = int(response.headers["Content-Length"])
+                if (size > mg.maxDownloadSize):
+                    print("[-] File is too large [" + str(size) + " bytes] to download " + url)
+                else:
+                    print("[+] Downloading file - [" + str(size) + " bytes] " + url)
+                    filename = str(url.split("/")[-1]) 
+                    urllib.urlretrieve(url, mg.saveDirectory + "/" + filename)
+                    mg.totalBytes += size
+                    
+            except:
+                print("[-] Timed out after " + str(mg.urlTimeout) + " seconds...can't reach url: " + url)
+            
+            mg.queue.task_done()
+
+
 class Metagoofil:
 
-    def __init__(self, domain, fileTypes, searchMax, downloadFileLimit, maxDownloadSize, saveDirectory, downloadFiles, saveLinks, delay, urlTimeout):
+    def __init__(self, domain, fileTypes, searchMax, downloadFileLimit, maxDownloadSize, saveDirectory, downloadFiles, saveLinks, delay, urlTimeout, numThreads):
         self.domain = domain
         self.fileTypes = fileTypes
         self.searchMax = searchMax
@@ -25,8 +55,20 @@ class Metagoofil:
         self.delay = delay
         self.totalBytes = 0
         self.urlTimeout = urlTimeout
+        
+        # Create queue and specify the number of worker threads.
+        self.queue = Queue.Queue() 
+        self.numThreads = numThreads
+
+        self.files = []  # Stores URLs with files
 
     def go(self):
+        # Kickoff the threadpool.
+        for i in range(self.numThreads):
+            thread = Worker()
+            thread.daemon = True
+            thread.start()
+
         if "ALL" in self.fileTypes:
             from itertools import product
             from string import ascii_lowercase
@@ -35,56 +77,45 @@ class Metagoofil:
 
         for filetype in self.fileTypes:
             # Search for the files to download
-            print "[*] Searching for " + str(self.searchMax) + " ." + filetype + " files and waiting " + str(self.delay) + " seconds between searches"
-            files = []
+            print("[*] Searching for " + str(self.searchMax) + " ." + filetype + " files and waiting " + str(self.delay) + " seconds between searches")
             query = "filetype:" + filetype + " site:" + self.domain
             for url in google.search(query, start=0, stop=self.searchMax, num=100, pause=self.delay):
-                files.append(url)
+                self.files.append(url)
             
             # Since google.search method retreives URLs in batches of 100, ensure the file list only contains the requested amount
-            if len(files) > self.searchMax:
-                files = files[:-(len(files) - self.searchMax)] 
+            if len(self.files) > self.searchMax:
+                self.files = self.files[:-(len(self.files) - self.searchMax)] 
                         
-            # Download files if specified with -w argument
+            # Download files if specified with -w switch
             if self.downloadFiles:
-                self.download(files)
+                self.download()
             
             # Otherwise, just display them
             else:
-                print "[*] Results: " + str(len(files)) + " ." + filetype + " files found"
-                for file in files:
-                    print file
+                print("[*] Results: " + str(len(self.files)) + " ." + filetype + " files found")
+                for file in self.files:
+                    print(file)
             
             # Save links to output to file
             if self.saveLinks:
                 self.f = open('html_links_' + get_timestamp() + '.txt', 'a')
-                for file in files:
+                for file in self.files:
                     self.f.write(file + "\n")
                 self.f.close()
         
         if self.downloadFiles:
-            print "[+] Total download: " + str(self.totalBytes) + " bytes / " + str(self.totalBytes / 1024) + " KB / " + str(self.totalBytes / (1024 * 1024)) + " MB"
+            print("[+] Total download: " + str(self.totalBytes) + " bytes / " + str(self.totalBytes / 1024) + " KB / " + str(self.totalBytes / (1024 * 1024)) + " MB")
                       
-    def download(self, files):
-        counter = 1
-        for url in files:        
-            if counter <= self.downloadFileLimit:
-                try:
-                    response = urllib2.urlopen(url, timeout=5)
-                    # Determine if file is small enough to download
-                    size = int(response.headers["Content-Length"])
-                    if (size > self.maxDownloadSize):
-                        print "[-] File is too large [" + str(size) + " bytes] to download " + url
-                    else:
-                        print "[+] Downloading file " + str(counter) + "/" + str(self.downloadFileLimit) + " - [" + str(size) + " bytes] " + url
-                        filename = str(url.split("/")[-1]) 
-                        urllib.urlretrieve(url, self.saveDirectory + "/" + filename)
-                        self.totalBytes += size
-                        counter += 1
-                except:
-                    print "[-] Timed out after " + str(self.urlTimeout) + " seconds...can't reach url: " + url            
-                    
-                               
+    def download(self):
+        self.counter = 1
+        for url in self.files:
+            if self.counter <= self.downloadFileLimit:
+                self.queue.put(url)
+                self.counter += 1
+
+        self.queue.join()
+
+
 def get_timestamp():
     now = time.localtime()
     timestamp = time.strftime('%Y%m%d_%H%M%S', now)
@@ -93,8 +124,8 @@ def get_timestamp():
 
 def csv_list(string):
     return string.split(',')                    
-   
-   
+
+ 
 if __name__ == "__main__":
     
     parser = argparse.ArgumentParser(description='Metagoofil - Search and Download Filetypes')
@@ -108,32 +139,36 @@ if __name__ == "__main__":
     parser.add_argument('-f', dest='saveLinks', action='store_true', default=False, help='Save the html links to html_links_<TIMESTAMP>.txt file')
     parser.add_argument('-e', dest='delay', action='store', type=float, default=7.0, help='Delay (in seconds) between searches.  If it\'s too small Google may block your IP, too big and your search may take a while.')
     parser.add_argument('-i', dest='urlTimeout', action='store', type=int, default=5, help='Number of seconds to wait before timeout for unreachable/stale pages (default 5)')
+    parser.add_argument('-r', dest='numThreads', action='store', type=int, default=8, help='Number of search threads (default is 8)')
 
     args = parser.parse_args()
 
     if not args.domain:
-        print "[!] Specify a domain with -d"
+        print("[!] Specify a domain with -d")
         sys.exit()
     if not args.fileTypes:
-        print "[!] Specify file types with -t"
+        print("[!] Specify file types with -t")
         sys.exit()
     if (args.downloadFileLimit > 0) and (args.downloadFiles is False):
-        print "[+] Adding -w for you"
+        print("[+] Adding -w for you")
         args.downloadFiles = True
     if args.saveDirectory:
-        print "[*] Downloaded files will be saved here: " + args.saveDirectory
+        print("[*] Downloaded files will be saved here: " + args.saveDirectory)
         if not os.path.exists(args.saveDirectory):
-            print "[+] Creating folder: " + args.saveDirectory
+            print("[+] Creating folder: " + args.saveDirectory)
             os.mkdir(args.saveDirectory)
     if args.delay < 0:
-        print "[!] Delay must be greater than 0"
+        print("[!] Delay must be greater than 0")
         sys.exit()
     if args.urlTimeout < 0:
-        print "[!] URL timeout (-i) must be greater than 0"
+        print("[!] URL timeout (-i) must be greater than 0")
+        sys.exit()
+    if args.numThreads < 0:
+        print("[!] Number of threads (-n) must be greater than 0")
         sys.exit()
 
     #print vars(args)
     mg = Metagoofil(**vars(args))
     mg.go()
 
-    print "[+] Done!"
+    print("[+] Done!")
